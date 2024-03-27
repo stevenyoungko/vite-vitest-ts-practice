8064616ba271ab71967923d51c6112d1c2f8b225
Steven Ko <steven.ko@shoplineapp.com>
2024-03-27 11:50:29 +0800

    feat: add sleep

---
+++ src/sleep/sleep.test.ts
@@ -0,0 +1,21 @@
+import { describe, expect, test } from 'vitest';
+import { sleep } from './sleep';
+
+describe('sleep', () => {
+  test('should await with sleep function using await', async () => {
+    const start = Date.now();
+    await sleep(3000);
+    const end = Date.now();
+
+    expect(end - start).toBeGreaterThanOrEqual(3000);
+  });
+
+  test('should await with sleep function using then', () => {
+    const start = Date.now();
+    sleep(3000).then(() => {
+      const end = Date.now();
+
+      expect(end - start).toBeGreaterThanOrEqual(3000);
+    });
+  });
+});
